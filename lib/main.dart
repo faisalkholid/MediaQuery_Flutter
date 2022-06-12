e9e49e4e6f62fb0fc237efe4091a22781ad1092a
faisalkholid <noah.faisal87@gmail.com>
2022-06-12 14:44:25 +0800

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,93 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final mediaQueryHeight = MediaQuery.of(context).size.height;
+    final mediaQueryWidth = MediaQuery.of(context).size.width;
+    final myAppBar = AppBar(
+      title: Text("Media Query"),
+    );
+    final bodyHeight = mediaQueryHeight -
+        myAppBar.preferredSize.height -
+        MediaQuery.of(context).padding.top;
+
+    final bool isLandscape =
+        MediaQuery.of(context).orientation == Orientation.landscape;
+    return Scaffold(
+      appBar: myAppBar,
+      body: Center(
+          child: (isLandscape)
+              ? Column(
+                  children: [
+                    Container(
+                      height: bodyHeight * 0.5,
+                      width: mediaQueryWidth,
+                      color: Colors.amber,
+                    ),
+                    Container(
+                      color: Colors.white,
+                      height: bodyHeight * 0.5,
+                      child: GridView.builder(
+                        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                            crossAxisCount: 2,
+                            mainAxisSpacing: 10,
+                            crossAxisSpacing: 10),
+                        itemCount: 100,
+                        itemBuilder: (context, index) {
+                          return GridTile(
+                            child: Container(
+                              color: Color.fromARGB(255, Random().nextInt(156),
+                                  Random().nextInt(156), Random().nextInt(156)),
+                            ),
+                          );
+                        },
+                      ),
+                    )
+                  ],
+                )
+              : Column(
+                  children: [
+                    Container(
+                      height: bodyHeight * 0.3,
+                      width: mediaQueryWidth,
+                      color: Colors.amber,
+                    ),
+                    Container(
+                      color: Colors.white,
+                      height: bodyHeight * 0.7,
+                      child: ListView.builder(
+                        itemCount: 100,
+                        itemBuilder: (context, index) {
+                          return ListTile(
+                            leading: CircleAvatar(),
+                            title: Text("Halo guys"),
+                          );
+                        },
+                      ),
+                    )
+                  ],
+                )),
+    );
+  }
+}
